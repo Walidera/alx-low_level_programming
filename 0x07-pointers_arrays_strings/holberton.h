4029e35b0953bc681ee872b06162082bd858c0f4
Walidera <113612429+Walidera@users.noreply.github.com>
2022-10-31 11:54:55 -0700

    Update holberton.h

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,9 +1,9 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
- * File: holberton.h
- * Auth: Firdaus
+ * File: Main.h
+ * Auth: Walidera
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x07-pointers_arrays_strings directory.
  */
